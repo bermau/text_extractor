429a1beb2188fa0c365c4e15c13bf8e2ab212244
bertrand <bertrand.maubert@ch-lepuy.fr>
2022-09-07 19:52:41 +0200

    Mises au point

--- map_for_find_all_begin_and_block.py
+++ map_for_find_all_begin_and_block.py
@@ -1,11 +1,17 @@
 # Fichier pour la mise au point.
 import lib_logstudy
-
+from lib_logstudy import newline
 i_file = r"data_in/demo_log_72_lines.log"
 
-AA = log_study.TextManipulator(i_file)
+AA = lib_logstudy.TextManipulator(i_file)
 AA.print_info()
+AA.remove_carriage_return()
+AA.cat()
 
-AA.find_all_begin_end_block(init_pattern="Task start:", end_pattern="Executing task")
+newline()
+
+select = AA.select_all_begin_end_block(init_pattern="Task start:", end_pattern="Executing task")
+
+
 
 
--- mise_au_point_lib_log_study.py
+++ mise_au_point_lib_log_study.py
@@ -14,23 +14,17 @@ AA = lib_logstudy.TextManipulator(i_file, encoding="utf-8")
 AA.print_info()
 
 #
-# AA.find_all_begin_end_block(init_pattern="Task start:", end_pattern="Executing task")
+# AA.get_all_begin_end_block(init_pattern="Task start:", end_pattern="Executing task")
 # AA.supRegex("0 ")
 # AA.supp_empty_lines()
 AA.remove_carriage_return()
-AA.cat_lines()
-# AA.get_first_ocurence_with_contexte("Dossier 20141709", before=2, after= 3)
-# Cette recherche ne ramène rien.
-print("Tests de : get_first_ocurence_with_contexte('Dossier 20141709'', before=2, after= 3)")
-display_lines(AA.get_first_ocurence_with_contexte("Dossier 20141709", before=2, after= 3))
-
+AA.cat()
 new_line()
 
-
-print("Tests de : sup_regex")
+print("Tests de : remove_regex")
 AA.replace_regex("ceci", "cela")
-AA.cat_lines()
+AA.cat()
 
-# AA.cat_lines()
+# AA.cat()
 # AA.print_info()
 
