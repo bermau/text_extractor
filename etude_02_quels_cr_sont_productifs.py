a1a9ed9872e875a9fad2b21b026bf3e976a05fa7
bertrand <bertrand.maubert.info@gmail.com>
2022-09-10 17:58:19 +0200

    fix 2 bugs : - adaptation for home (location of another module + encoding) - removed num from a line

--- etude_02_quels_cr_sont_productifs.py
+++ etude_02_quels_cr_sont_productifs.py
@@ -6,6 +6,7 @@ import lib_logstudy
 from lib_logstudy import display_lines, mark_line_block, TextManipulator, NumberedLine
 from pprint import pprint
 import glob
+
 # Vers la ligne 21634,il y a une suite d'impression.
 
 
@@ -14,6 +15,7 @@ sys.path.append("../../HI_31_outils_divers")
 from lib_bm_utils import readkey, title
 
 default_file = r"./data_in/glimscron4_20220830_221610.log"
+
 
 # Essai de modification d'une fonction à la classe TextManipulator
 def get_all_begin_end_block(self, init_pattern, end_pattern, start_line=0,
@@ -24,7 +26,7 @@ def get_all_begin_end_block(self, init_pattern, end_pattern, start_line=0,
                                               skip=start_line, before=before, after=after)
     buf = []
     for block in gene:
-        if len(block) != 7:    # NOUVEAU
+        if len(block) != 7:  # NOUVEAU
             # On va créer un sous-objet
             # keep only the first line of the block
             block_a_enregistrer = [block[0]]
@@ -43,6 +45,7 @@ def get_all_begin_end_block(self, init_pattern, end_pattern, start_line=0,
             else:
                 buf.extend(block_a_enregistrer)
     return buf
+
 
 lib_logstudy.TextManipulator.get_all_begin_end_block = get_all_begin_end_block
 
@@ -63,11 +66,12 @@ def select_lines(self, begin_line_num, end_line_num):
                 status = 1
     self.n_lines = buf
 
+
 lib_logstudy.TextManipulator.select_lines = select_lines
 
 
-def traitement(i_file = default_file):
-    title ("Etude du log de "+ i_file)
+def traitement(i_file=default_file):
+    title("Etude du log de " + i_file)
     AA = lib_logstudy.TextManipulator(i_file, encoding="latin1")
     AA.remove_carriage_return()
     # AA.cat()
@@ -78,33 +82,10 @@ def traitement(i_file = default_file):
     # AA.sce(15000, 15500)
     # AA.select_lines(21634, 21700)
     AA.cat()
-    print("Rescriction à lignes ", len(AA))
-    #
-    # # Créer une méthode pour extraire une sous chaine dans la regex
-    # # AA.head(30)
-    # # AA.cat()
-    # # title("Elimination début de ligne")
-    #
-    # AA.replace_regex(r".* '", '')
-    # # AA.cat()
-    # AA.replace_regex(r"'", '')
-    # # title("La liste des CR à étudier")
-    # # AA.cat(quiet=True)
-    # title ("Nombre de lignes " + str(len(AA)))
-    #
-    # # Il faudrait récupérer une liste. Je crée l'outil adéquat AA.to_list()
-    # list_des_cr = AA.to_list()
-    # set_de_cr = set(list_des_cr)
-    #
-    # title("Les CR activés sont donc...")
-    # pprint(set_de_cr)
-    #
-    # print("leur nombre est de ", len(set_de_cr))
-    #
     readkey("t...")
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     traitement()
 # BOUCLE
 #     lst_fichiers_a_traiter  = sorted(glob.glob("./data_in/glims*.log"))
